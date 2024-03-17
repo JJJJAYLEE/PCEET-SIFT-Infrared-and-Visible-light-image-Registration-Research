4a9a778664ea160180d718a780d6884d86b77cf4
JJJJAYLEE <152461128+JJJJAYLEE@users.noreply.github.com>
2024-03-17 15:25:03 +0800

    Create README.m

---
+++ README.m
@@ -0,0 +1,69 @@
+# Infrared and Visible light image Registration Research Based on the Combination of Phase Consistency and Edge Extreme Points with SIFT Algorithm
+Infrared and Visible light image Registration Research Based on the Combination of Phase Consistency and Edge Extreme Points with SIFT Algorithm
+
+说明：
+main.py脚本为主脚本，运行main.py脚本时,输入参数--des_way 2 表示采用pytorch张量形式并用cuda计算，可以加快计算速度，强烈建议采用。
+
+数据准备：data文件夹的images文件夹用来存放原始图片，其中images1存放原始红外图片，images2存放原始可见光图片
+
+重要脚本说明：
+
+main.py——项目主脚本，
+
+affine,py——配准时进行仿射变换的脚本
+
+count_corners_and_angles.py——计算特征点和主方向脚本
+
+dataloads.py——加载数据脚本
+
+descriptor_and_match.py——计算描述符和粗匹配脚本
+
+registration.py——配准主要脚本
+
+show.py——可视化脚本
+
+注：各脚本具体作用以实际代码为准，此处只提供主要作用
+
+项目运行注意事项：
+请按requirement.txt内的要求安装外部库
+
+
+
+注意：此代码为李杰(2973137145@qq.com)原创，未经本人允许不准用于其他地方，暂时只用于论文审查使用, project.rar文件为代码文件，需要输入解压密码才能解压，需要采用WinRAR软件进行解压，其他解压软件可能解压失败
+
+
+Instructions:
+
+The main script is main.py, which serves as the primary script for the project. When running main.py, input parameter 
+--des_way 2 indicates the use of PyTorch tensors for faster computation with CUDA. It is highly recommended to utilize 
+this option for accelerated processing.
+
+Data Preparation: The images folder within the data directory is designated for storing original images. Specifically, 
+images1 contains the original infrared images, while images2 holds the original visible light images.
+
+Key Script Descriptions:
+
+main.py: Main script of the project.
+
+affine.py: Script for affine transformations during registration.
+
+count_corners_and_angles.py: Script for computing feature points and main directions.
+
+dataloads.py: Script for loading data.
+
+descriptor_and_match.py: Script for computing descriptors and performing coarse matching.
+
+registration.py: Main script for registration operations.
+
+show.py: Script for visualization.
+
+Project Execution Notes:
+
+Please ensure the installation of external libraries as specified in requirement.txt.
+
+Note: This code is original work by Jie Li (2973137145@qq.com). Use for purposes other than academic review without the 
+author's permission is prohibited. The project.rar file is a code file, you need to enter the decompression password to decompress, you need to use WinRAR software to decompress, other decompression software may fail to decompress
+
+
+
+
